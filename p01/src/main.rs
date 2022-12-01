df5dbfef5247cf0737cf3752c76b69636c615529
Gerard <gerard@wetransfer.com>
2022-12-01 10:47:26 +0100

    Fix part 2

--- p01/src/main.rs
+++ p01/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     println!("Part 1: {}", part1("input"));
+    println!("Part 2: {}", part2("input"));
 }
 
 fn part1(file: &'static str) -> u32 {
@@ -22,6 +23,27 @@ fn part1(file: &'static str) -> u32 {
 }
 
 #[test]
-fn test_input() {
+fn test_part1() {
     assert_eq!(part1("test_input"), 24000)
+}
+
+fn part2(file: &'static str) -> u32 {
+    let angels = fs::read_to_string(file).unwrap();
+    let mut totals = vec![];
+
+    for angel in angels.split("\n\n") {
+        let total: u32 = angel
+            .split_terminator("\n")
+            .map(|n| n.parse::<u32>().unwrap())
+            .sum();
+
+        totals.push(total)
+    }
+    totals.sort_by(|a, b| b.cmp(a));
+    totals[0..3].iter().sum()
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2("test_input"), 45000)
 }
