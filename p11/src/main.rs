51408b8829e4f52039c779deafed59bdb668f45a
Gerard <gerard@wetransfer.com>
2022-12-15 15:55:11 +0100

    I hate aoc

--- p11/src/main.rs
+++ p11/src/main.rs
@@ -1,24 +1,22 @@
-use num_bigint::{ToBigUint, BigUint};
 use std::fs;
 
 type Monkeys = Vec<Monkey>;
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<u64>,
-    big_uint_items: Vec<BigUint>,
+    items: Vec<u128>,
     operation: String,
-    test: u64,
+    test: u128,
     test_positive_id: usize,
     test_negative_id: usize
 }
 
 impl Monkey {
-    pub fn determine_worry_level(&mut self) {
+    pub fn determine_worry_level(&mut self, div: u128, modulo: u128) {
         let operands: Vec<&str> = self.operation.split(" ").collect();
 
         for item in &mut self.items {
-            let n = operands[2].parse::<u64>().unwrap_or(*item);
+            let n = operands[2].parse::<u128>().unwrap_or(*item);
 
             match operands[1] {
                 "*" => *item *= n,
@@ -26,41 +24,13 @@ impl Monkey {
                 _ => panic!("Invalid operation")
             }
 
-            *item /= 3
+            *item = *item / div;
+            *item = *item % modulo;
         }
     }
 
-    pub fn determine_next_worry_level(&mut self) {
-        let operands: Vec<&str> = self.operation.split(" ").collect();
-
-        for item in &mut self.big_uint_items {
-            let n: BigUint;
-            match operands[2].parse::<u64>() {
-                Ok(x) => n = x.to_biguint().unwrap(),
-                Err(_) => n= 1.to_biguint().unwrap()
-            }
-
-            match operands[1] {
-                "*" => *item *= n,
-                "+" => *item += n,
-                _ => panic!("Invalid operation")
-            }
-        }
-    }
-
-    pub fn throw_id(&self, value: u64) -> usize {
+    pub fn throw_id(&self, value: u128) -> usize {
         if value % self.test == 0 {
-            self.test_positive_id
-        } else {
-            self.test_negative_id
-        }
-    }
-
-    pub fn next_throw_id(&self, value: &BigUint) -> usize {
-        let comp = 0.to_biguint().unwrap();
-        let t_biguint = self.test.to_biguint().unwrap();
-
-        if value % t_biguint == comp {
             self.test_positive_id
         } else {
             self.test_negative_id
@@ -75,15 +45,20 @@ fn main() {
 
 fn part1(file: &'static str) -> usize {
     let mut monkeys = parse(file);
+    trick(&mut monkeys, 20, 3)
+}
+
+fn trick(monkeys: &mut Monkeys, steps: usize, division: u128) -> usize {
     let mut move_items = vec![vec![]; monkeys.len()];
     let mut inspection_count = vec![0; monkeys.len()];
+    let total_mods: u128 = monkeys.iter().map(|n| n.test).product();
 
-    for _ in 0..20 {
+    for _ in 0..steps {
         for monkey_id in 0..monkeys.len() {
             let monkey = &mut monkeys[monkey_id];
             monkey.items.append(&mut move_items[monkey_id]);
             inspection_count[monkey_id] += monkey.items.len();
-            monkey.determine_worry_level();
+            monkey.determine_worry_level(division, total_mods);
 
             while let Some(item) = monkey.items.pop() {
                 let id = monkey.throw_id(item);
@@ -104,27 +79,7 @@ fn test_part1() {
 
 fn part2(file: &'static str) -> usize {
     let mut monkeys = parse(file);
-    let mut move_items = vec![vec![]; monkeys.len()];
-    let mut inspection_count = vec![0; monkeys.len()];
-
-    for c in 0..10_000 {
-        println!("-- CYCLE {}", c);
-        for monkey_id in 0..monkeys.len() {
-            let monkey = &mut monkeys[monkey_id];
-            monkey.big_uint_items.append(&mut move_items[monkey_id]);
-            inspection_count[monkey_id] += monkey.big_uint_items.len();
-            monkey.determine_next_worry_level();
-
-            while let Some(item) = monkey.big_uint_items.pop() {
-                let id = monkey.next_throw_id(&item);
-
-                move_items[id].push(item)
-            }
-        }
-    }
-
-    inspection_count.sort();
-    inspection_count[monkeys.len() - 1] * inspection_count[monkeys.len() - 2]
+    trick(&mut monkeys, 10_000, 1)
 }
 #[test]
 fn test_part2() {
@@ -137,7 +92,6 @@ fn parse(file: &'static str) -> Monkeys {
 
     for monkey in monkey_text.split_terminator("\n\n") {
         let mut items = vec![];
-        let mut big_uint_items = vec![];
         let mut operation = String::new();
         let mut test = 0;
         let mut test_positive_id = 0;
@@ -149,8 +103,7 @@ fn parse(file: &'static str) -> Monkeys {
             match key {
                 "  Starting items" => {
                     for item in val.split(", ") {
-                        let n = item.trim().parse::<u64>().unwrap();
-                        big_uint_items.push(n.to_biguint().unwrap());
+                        let n = item.trim().parse::<u128>().unwrap();
                         items.push(n);
                     }
                 },
@@ -162,7 +115,7 @@ fn parse(file: &'static str) -> Monkeys {
                 "  Test" => {
                     let op = val.trim();
                     let (_, method) = op.split_once("divisible by ").unwrap();
-                    test = method.parse::<u64>().unwrap();
+                    test = method.parse::<u128>().unwrap();
                 },
                 "    If true" => {
                     test_positive_id = (
@@ -181,7 +134,6 @@ fn parse(file: &'static str) -> Monkeys {
         monkeys.push(Monkey {
             items: items,
             operation: operation,
-            big_uint_items: big_uint_items,
             test: test,
             test_positive_id: test_positive_id,
             test_negative_id: test_negative_id
