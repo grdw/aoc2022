58603605139093a3962991f129b3c02417ba3dc5
Gerard <gerard@wetransfer.com>
2022-12-16 17:27:37 +0100

    First attempt

--- p13/src/main.rs
+++ p13/src/main.rs
@@ -22,22 +22,88 @@ impl GNode {
     pub fn add(&mut self, val: u8) {
         self.children.push(GNode::node(val));
     }
+
+    pub fn comparible(&self) -> bool {
+        self.children.len() == 0 && self.value.is_some()
+    }
 }
 
 fn main() {
     println!("Part 1: {}", part1("input"));
-    println!("Part 2: {}", part1("input"));
+    println!("Part 2: {}", part2("input"));
 }
 
 fn part1(file: &'static str) -> usize {
     let parsed = fs::read_to_string(file).unwrap();
-    for group in parsed.split_terminator("\n\n") {
+    let mut total = 0;
+    for (i, group) in parsed.split_terminator("\n\n").enumerate() {
         let (left, right) = group.split_once("\n").unwrap();
         let l_tree = parse_tree(left);
         let r_tree = parse_tree(right);
-    }
+        let mut ordered = false;
+        println!("\nPAIR: {}", i + 1);
+        traverse(&l_tree, &r_tree, &mut ordered);
 
-    0
+        if ordered {
+            println!("🎉 {}", i);
+            total += (i + 1)
+        }
+    }
+    total
+}
+
+fn traverse(l: &GNode, r: &GNode, ordered: &mut bool) {
+    let mut i = 0;
+
+    loop {
+        match (l.children.get(i), r.children.get(i)) {
+            (Some(l_child), Some(r_child)) => {
+                println!("\n{:?} --- 🥊 --- {:?}", l_child, r_child);
+
+                if l_child.comparible() && r_child.comparible() {
+                    let l_val = l_child.value.unwrap();
+                    let r_val = r_child.value.unwrap();
+                    if l_val < r_val {
+                        *ordered = true
+                    }
+                } else if l_child.comparible() {
+                    let l_val = l_child.value.unwrap();
+                    if let Some(n) = r_child.children.get(0) {
+                        let r_val = n.value.unwrap_or(l_val);
+                        if l_val < r_val {
+                            *ordered = true
+                        }
+                    }
+                    break;
+                } else if r_child.comparible() {
+                    let r_val = r_child.value.unwrap();
+                    if let Some(n) = l_child.children.get(0) {
+                        let l_val = n.value.unwrap_or(r_val);
+                        if l_val < r_val {
+                            *ordered = true
+                        }
+                    }
+                    break;
+                } else {
+
+                }
+
+                traverse(l_child, r_child, ordered)
+            },
+            (Some(l_child), None) => {
+                println!("{}", "Only a left child");
+            },
+            (None, Some(r_child)) => {
+                if r_child.children.len() == 0 {
+                    *ordered = true;
+                }
+                println!("{}", "Only a right child");
+            },
+            (None, None) => break
+        }
+
+        i += 1;
+    }
 }
 
 fn parse_tree(line: &str) -> GNode {
@@ -73,7 +139,7 @@ fn add_child(s: &mut String, depth: &usize, root: &mut GNode) {
     let value = s.parse::<u8>().unwrap();
     let mut n = root;
 
-    for i in 0..depth {
+    for _ in 0..depth {
         let len = n.children.len() - 1;
         n = n.children.get_mut(len).unwrap();
     }
@@ -88,7 +154,7 @@ fn add_blank(depth: &usize, root: &mut GNode) {
     let depth = depth - 1;
     let mut n = root;
 
-    for i in 0..depth {
+    for _ in 0..depth {
         let len = n.children.len() - 1;
         n = n.children.get_mut(len).unwrap();
     }
