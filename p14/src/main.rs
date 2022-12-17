ece3e7d3c91460fefda6865ae5d4876495edbadb
grdw <gerard@grdw.nl>
2022-12-17 14:04:07 +0100

    Start with some setup

---
+++ p14/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+fn main() {
+    println!("Part 1: {}", part1("input"));
+    println!("Part 2: {}", part2("input"));
+}
+
+fn part1(file: &'static str) -> usize {
+    0
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1("test_input"), 24);
+}
+
+fn part2(file: &'static str) -> usize {
+    0
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2("test_input"), 24);
+}
