70215808c365f02475e7cca381eb4927be53abd3
grdw <gerard@grdw.nl>
2022-12-18 13:00:21 +0100

    Learn the basics of diamonds

--- p15/src/main.rs
+++ p15/src/main.rs
@@ -14,6 +14,8 @@ struct Point {
     t: PointType
 }
 
+type Grid = Vec<Vec<char>>;
+
 impl Point {
     fn sensor(x: i32, y: i32) -> Point {
         Point { x: x, y: y, t: PointType::Sensor }
@@ -33,7 +35,13 @@ fn main() {
 
 fn part1(file: &'static str, pos: usize) -> usize {
     let pairs = parse(file);
-    debug(&pairs);
+    //let grid = to_grid(&pairs);
+    draw_diamond(1);
+    draw_diamond(2);
+    draw_diamond(3);
+    draw_diamond(4);
+    draw_diamond(5);
+    //debug(&grid);
     0
 }
 
@@ -67,8 +75,15 @@ fn parse(file: &'static str) -> PointPairs {
     }).collect()
 }
 
-fn debug(pairs: &PointPairs) {
-    let mut grid: Vec<Vec<char>> = vec![];
+fn debug(grid: &Grid) {
+    println!("");
+    for line in grid {
+        println!("{}", line.into_iter().collect::<String>());
+    }
+}
+
+fn to_grid(pairs: &PointPairs) -> Grid {
+    let mut grid: Grid = vec![];
     let mut min_y = i32::MAX;
     let mut max_y = 0;
     let mut min_x = i32::MAX;
@@ -95,17 +110,145 @@ fn debug(pairs: &PointPairs) {
         grid.push(s);
     }
 
+    let h = grid.len();
+    let w = grid[0].len();
     for (sensor, beacon) in pairs {
         let sy = (sensor.y - min_y) as usize;
         let sx = (sensor.x - min_x) as usize;
         let by = (beacon.y - min_y) as usize;
         let bx = (beacon.x - min_x) as usize;
 
+        let dist: usize = (
+            (beacon.x - sensor.x).abs() +
+            (beacon.y - sensor.y).abs()
+        ) as usize;
+
+        println!("\n\n==== DEBUG START");
+        println!("-------- DISTANCE {:?}", dist);
+        let mut n = 1;
+        loop {
+            // 2 is arbitary right now but it could by any N
+            if n > dist {
+                break;
+            }
+
+            println!("-----");
+            println!("{}", n);
+
+            //if sy + n < h  {
+            //    grid[sy + n][sx] = '#'
+            //}
+
+            //if sy >= n {
+            //    grid[sy - n][sx] = '#'
+            //}
+
+            //if sx + n < w {
+            //    grid[sy][sx + n] = '#'
+            //}
+
+            //if sx >= n {
+            //    grid[sy][sx - n] = '#'
+            //}
+
+            for p in 0..n {
+                let tx = 1;
+                let ty = 1 + p;
+                println!("{} {}", tx, ty);
+                println!("-{} {}", tx, ty);
+                println!("{} -{}", tx, ty);
+                println!("-{} -{}", tx, ty);
+            }
+            //for m in 1..=n {
+            //    println!("-{}, {}", m, m);
+            //    println!("{}, -{}", m, m);
+            //    println!("{}, {}", m, m);
+            //    println!("-{}, -{}", m, m);
+            //    //if sy >= n {
+            //    //    grid[sy - m][sx - m] = '#';
+            //    //    grid[sy + m][sx - m] = '#';
+            //    //    grid[sy + m][sx + m] = '#';
+            //    //    grid[sy - m][sx + m] = '#';
+            //    //}
+            //}
+            //for tx in 0..=i {
+            //    //println!("COORD: {}", i, j);
+            //}
+            //(sensor.x - 1, sensor.y)
+            //(sensor.x + 1, sensor.y)
+            //(sensor.x, sensor.y)
+            //(sensor.x, sensor.y)
+            //
+            n += 1;
+
+        }
+
         grid[sy][sx] = 'S';
         grid[by][bx] = 'B';
+
+        println!("S: ({},{}), B: ({},{})", sensor.x, sensor.y, beacon.x, beacon.y);
+        println!("Delta: ({},{})", sensor.x - beacon.x, sensor.y - beacon.y);
+
     }
 
-    for line in grid {
-        println!("{}", line.into_iter().collect::<String>());
+    grid
+}
+
+fn draw_diamond(size: usize) {
+    //let g_size = 10;
+
+    // Makes a simple 10 by 10 grid:
+    //let mut v: Grid = vec![];
+    //for h in 0..=g_size {
+    //    let mut sub = vec![];
+    //    for w in 0..=g_size {
+    //        sub.push('.');
+    //    }
+    //    v.push(sub);
+    //}
+
+    // Draws the S in the middle;
+    //v[g_size / 2][g_size / 2] = 'S';
+
+    //for k in 0..size {
+    //    let l = ((size + 1) / 2) - k - 1;
+    //    println!("{}", l);
+    //    if l > 0 {
+    //        v[k][l] = '#';
+    //    }
+    //}
+
+    for i in 0..=size {
+        let num_spaces = (size + 1) - i - 1;
+        for n in 0..num_spaces {
+            print!(" ");
+        }
+
+        for k in (0..=i).rev() {
+            print!("#");
+        }
+
+        for j in 1..=i {
+            print!("#");
+        }
+        println!("");
     }
+
+    for i in (0..=size-1).rev() {
+        let num_spaces = (size + 1) - i - 1;
+        for n in 0..num_spaces {
+            print!(" ");
+        }
+
+        for k in (0..=i).rev() {
+            print!("#");
+        }
+
+        for j in 1..=i {
+            print!("#");
+        }
+        println!("");
+    }
+
+    //debug(&v);
 }
