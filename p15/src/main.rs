6e67fe0fb3fd4e3fe4755ad745b822b4fd96f728
grdw <gerard@grdw.nl>
2022-12-18 21:14:43 +0100

    Resolve part 2 in a nightmare way

--- p15/src/main.rs
+++ p15/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::cmp;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use regex::Regex;
 
 #[derive(Debug)]
@@ -45,7 +45,6 @@ fn test_part1() {
 
 // Fucking nightmare code
 fn count_bonqs(pairs: &PointPairs, pos: usize) -> usize {
-    let mut bonq_counts: HashSet<(isize, isize)> = HashSet::new();
     let mut lines = vec![];
 
     for (sensor, beacon) in pairs {
@@ -73,31 +72,103 @@ fn count_bonqs(pairs: &PointPairs, pos: usize) -> usize {
 
             line_squash(&mut lines, (start, end));
         }
+
+        merge(&mut lines);
     }
 
-    (lines[0].0..lines[1].1).len()
+    (lines[0].0..lines[0].1).len()
 }
 
 fn line_squash(lines: &mut Vec<(i32, i32)>, point: (i32, i32)) {
-    for i in 0..lines.len() {
+    let len = lines.len();
+
+    for i in 0..len {
         let mut l = lines.get_mut(i).unwrap();
 
-        if point.0 >= l.0 && point.1 <= l.1 {
+        if is_wrapped(&point, l) {
             return;
-        } else if point.0 < l.0 && point.1 > l.1 {
+        } else if is_wrapped(l, &point) {
             l.0 = point.0;
             l.1 = point.1;
             return;
-        } else if point.0 <= l.1 && point.0 >= l.0 {
+        } else if is_start(point.0, l) {
             l.1 = point.1;
             return;
-        } else if point.1 < l.1 && point.1 >= l.0 {
+        } else if is_start(point.1, l) {
             l.0 = point.0;
             return;
         }
     }
 
     lines.push(point);
+}
+
+fn is_wrapped(p1: &(i32, i32), p2: &(i32, i32)) -> bool {
+    p1.0 >= p2.0 && p1.1 <= p2.1
+}
+
+fn is_start(x: i32, p: &(i32, i32)) -> bool {
+    x <= p.1 && x >= p.0
+}
+
+fn merge(lines: &mut Vec<(i32, i32)>) {
+    lines.sort();
+
+    let len = lines.len();
+
+    for i in 0..len {
+        for j in (i + 1)..len {
+            let left = lines[i];
+            let right = lines[j];
+
+            if is_start(left.1, &right) {
+                lines[i].1 = right.1;
+                // Fucking ugly hack, but it works :')
+                lines[j] = (0, 0);
+            }
+        }
+    }
+
+    lines.retain(|(a, b)| !(*a == 0 && *b == 0));
+}
+
+#[test]
+fn test_line_squash_merge() {
+    let mut lines = vec![(-1, 2), (7, 10)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 2);
+
+    let mut lines = vec![(-1, 7), (7, 10)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 1);
+    assert_eq!(lines[0], (-1, 10));
+
+    let mut lines = vec![(7, 10), (0, 7)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 1);
+    assert_eq!(lines[0], (0, 10));
+
+    let mut lines = vec![(1,2), (3,4), (2,3)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 1);
+    assert_eq!(lines[0], (1,4));
+
+    let mut lines = vec![(1,2), (3,4), (2,3), (4,9)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 1);
+    assert_eq!(lines[0], (1,9));
+
+    let mut lines = vec![(1,2), (3,4), (2,3), (8,9)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 2);
+    assert_eq!(lines[0], (1,4));
+    assert_eq!(lines[1], (8,9));
+
+    let mut lines = vec![(1,2), (3,4), (2,3), (8,9), (9,12)];
+    merge(&mut lines);
+    assert_eq!(lines.len(), 2);
+    assert_eq!(lines[0], (1,4));
+    assert_eq!(lines[1], (8,12));
 }
 
 #[test]
@@ -112,12 +183,6 @@ fn test_line_squash() {
     line_squash(&mut lines, (5, 10));
     assert_eq!(lines.len(), 1);
     assert_eq!(lines[0], (-1, 10));
-
-    // Squash on the left-side (no full merge)
-    let mut lines = vec![(-1, 5), (7,10)];
-    line_squash(&mut lines, (5, 7));
-    assert_eq!(lines.len(), 2);
-    assert_eq!(lines[0], (-1, 7));
 
     // Squash on the left-side
     let mut lines = vec![(1, 5)];
@@ -144,7 +209,7 @@ fn test_line_squash() {
     assert_eq!(lines[0], (0, 10));
 }
 
-fn part2(file: &'static str, max: isize) -> usize {
+fn part2(file: &'static str, max: isize) -> i32 {
     let pairs = parse(file);
     tuning_frequency(&pairs, max)
 }
@@ -154,12 +219,9 @@ fn test_part2() {
     assert_eq!(part2("test_input", 20), 56000011);
 }
 
-fn tuning_frequency(pairs: &PointPairs, max: isize) -> usize {
+fn tuning_frequency(pairs: &PointPairs, max: isize) -> i32 {
+    let mut m: HashMap<isize, Vec<(i32, i32)>> = HashMap::new();
     let mut tuning_frequency = 0;
-    let mut xrange = (0, max);
-    let mut yrange = (0, max);
-    let mut dxrange = (0, max);
-    let mut dyrange = (0, max);
 
     for (sensor, beacon) in pairs {
         let sy = sensor.y;
@@ -173,22 +235,32 @@ fn tuning_frequency(pairs: &PointPairs, max: isize) -> usize {
 
         let mut ty = -(dist as isize);
 
-        println!("----");
-        //for i in top.chain(bottom) {
-        //    let start = cmp::max(-i + sx, 0);
-        //    let end = cmp::min(i + sx, max as i32);
-        //    let range = start..=end;
-        //    let cy = ty + (sy as isize);
-        //    ty += 1;
+        for i in top.chain(bottom) {
+            let cy = ty + (sy as isize);
 
-        //    if cy < 0 || cy > max {
-        //        continue
-        //    }
+            ty += 1;
 
-        //    println!("{:?} {:?}", cy, range);
-        //}
+            if cy < 0 || cy > max {
+                continue
+            }
+
+            let cxs = cmp::max(-i + sx, 0);
+            let cxe = cmp::min(i + sx, max as i32) + 1;
+
+            m.entry(cy)
+                .and_modify(|mut v| {
+                    line_squash(&mut v, (cxs, cxe));
+                    merge(&mut v)
+                })
+                .or_insert(vec![(cxs, cxe)]);
+        }
     }
 
+    m.retain(|_, val| val[0] != (0, (max as i32) + 1));
+
+    for (y, x_lines) in m {
+        tuning_frequency = x_lines[0].1 * 4_000_000 + (y as i32)
+    }
     tuning_frequency
 }
 
