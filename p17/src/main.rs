b597382ffb3f8505c2d4c083e0f9b893840a5d36
grdw <gerard@grdw.nl>
2022-12-21 12:36:42 +0100

    Update the debug code

--- p17/src/main.rs
+++ p17/src/main.rs
@@ -54,7 +54,6 @@ fn part1(file: &'static str) -> usize {
 
             // The wind should push the latest rock to whichever
             // direction
-
             if can_push_wind_right(&rock_coords, jet) {
                 println!("🍇 RIGHT PUSH >");
                 push_wind_right(rock_coords.last_mut().unwrap())
@@ -101,10 +100,12 @@ fn can_fall(coords: &Vec<Coords>) -> bool {
 
     for i in 0..l {
         for (y, x) in &coords[i] {
-            if *y == max_y - 1 {
-                if exes.contains(&x)  {
-                    fall = false
-                }
+            if *y != max_y - 1 {
+                continue
+            }
+
+            if exes.contains(&x)  {
+                fall = false
             }
         }
     }
@@ -213,14 +214,14 @@ fn debug_chamber(coords: &Vec<Coords>) {
     for _ in 0..=height {
         let mut chamber_line = vec![];
         for _ in 0..CHAMBER_WIDTH {
-            chamber_line.push('.');
+            chamber_line.push('⬜');
         }
         chamber.push(chamber_line);
     }
 
     for coords in coords {
         for (y, x) in coords {
-            chamber[height - *y][*x] = '#';
+            chamber[height - *y][*x] = '⬛';
         }
     }
 
