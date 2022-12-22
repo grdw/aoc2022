9130aafa7fc4b8f3a5392b08aa699fa7b74614dc
grdw <gerard@grdw.nl>
2022-12-22 22:53:39 +0100

    The classic, example is okay, but number too low

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -17,14 +17,127 @@ fn main() {
 
 fn part1(file: &'static str) -> usize {
     let (map, instr) = parse(file);
-    println!("{:?}", map);
-    println!("{:?}", instr);
-    0
+    let mut dir = '>';
+    let mut start = Point {
+        x: map[0].x,
+        y: map[0].y,
+        wall: false
+    };
+
+    for (steps, rotation) in &instr {
+        walk(&map, &mut start, *steps, dir);
+
+        dir = match (rotation, dir) {
+            ('R', '>') | ('L', '<') => 'v',
+            ('R', 'v') | ('L', '^') => '<',
+            ('R', '<') | ('L', '>') => '^',
+            ('R', '^') | ('L', 'v') => '>',
+            (_, _)                  => dir // stay the same
+        }
+    }
+
+    let facing_value = match dir {
+        '>' => 0,
+        'v' => 1,
+        '<' => 2,
+        '^' => 3,
+        _ => panic!("Invalid direction")
+    };
+
+    println!("{:?} {}", start, facing_value);
+    (1000 * (start.y + 1)) + (4 * (start.x + 1)) + facing_value
+}
+
+fn walk(map: &MonkeyMap, start: &mut Point, steps: u8, dir: char) {
+    let n_min_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).min().unwrap();
+    let n_max_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).max().unwrap();
+    let n_min_x = map.iter().filter(|p| p.y == start.y).map(|p| p.x).min().unwrap();
+    let n_max_x = map.iter().filter(|p| p.y == start.y).map(|p| p.x).max().unwrap();
+    let height = (n_max_y + 1) - n_min_y;
+    let width = (n_max_x + 1) - n_min_x;
+
+    for _ in 0..steps {
+        let mut next_wall = false;
+        match dir {
+            'v' => {
+                let next = n_min_y + (start.y + 1) % height;
+
+                next_wall = map
+                    .iter()
+                    .find(|p| p.x == start.x && p.y == next && p.wall)
+                    .is_some();
+
+                if next_wall {
+                    break;
+                }
+
+                //println!("v {:?}", start);
+                start.y = next;
+            },
+            '<' => {
+                let next = if start.x == 0 {
+                    n_min_x + n_max_x % width
+                } else {
+                    n_min_x + (start.x - 1) % width
+                };
+
+                next_wall = map
+                    .iter()
+                    .find(|p| p.x == next && p.y == start.y && p.wall)
+                    .is_some();
+
+                if next_wall {
+                    break;
+                }
+                //println!("< {:?}", start);
+                start.x = next;
+            },
+            '^' => {
+                let next = if start.y == 0 {
+                    n_min_y + n_max_y % height
+                } else {
+                    n_min_y + (start.y - 1) % height
+                };
+
+                next_wall = map
+                    .iter()
+                    .find(|p| p.x == start.x && p.y == next && p.wall)
+                    .is_some();
+
+                if next_wall {
+                    break;
+                }
+                //println!("^ {:?}", start);
+                start.y = next;
+            },
+            '>' => {
+                let next = n_min_x + (start.x + 1) % width;
+
+                next_wall = map
+                    .iter()
+                    .find(|p| p.x == next && p.y == start.y && p.wall)
+                    .is_some();
+
+                if next_wall {
+                    break;
+                }
+
+                start.x = next;
+                //println!("> {:?}", start);
+            },
+            _ => panic!("BOOM!")
+        }
+
+    }
 }
 
 #[test]
 fn test_part1() {
-    assert_eq!(part1("test_input"), 1);
+    assert_eq!(part1("test_input"), 6032);
+    assert_eq!(part1("test_input2"), 4046);
+    assert_eq!(part1("test_input3"), 9057);
+    assert_eq!(part1("test_input4"), 12043);
+    assert_eq!(part1("test_input5"), 5006);
 }
 
 fn part2(file: &'static str) -> usize {
