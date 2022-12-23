e33ea34d32f454d1274f02b47ee683e287a3c167
grdw <gerard@grdw.nl>
2022-12-23 18:53:12 +0100

    Example succeeds

--- p23/src/main.rs
+++ p23/src/main.rs
@@ -7,28 +7,18 @@ const TRANSFORMATIONS: [(isize, isize); 8] = [
     (-1, 1),  (0, 1),  (1, 1)
 ];
 
-#[derive(Debug, Clone)]
-enum Direction {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST,
-    IDLE
-}
-
 #[derive(Debug)]
 struct Elf {
     id: usize,
     x: isize,
-    y: isize,
-    considered_directions: VecDeque<Direction>
+    y: isize
 }
 
 impl Elf {
     fn no_elfs_in_dirs(&self, elfs: &Vec<Elf>) -> Vec<u8> {
         let mut comb = vec![];
 
-        for (i, (tx, ty)) in TRANSFORMATIONS.iter().enumerate() {
+        for (tx, ty) in &TRANSFORMATIONS {
             let elf_tx = self.x + tx;
             let elf_ty = self.y + ty;
             let mut elf_present = false;
@@ -52,52 +42,49 @@ impl Elf {
         comb
     }
 
-    fn to_coords(&self, direction: Direction) -> (isize, isize) {
+    fn to_coords(&self, direction: &char) -> (isize, isize) {
         match direction {
-            Direction::NORTH => (self.x, self.y - 1),
-            Direction::SOUTH => (self.x, self.y + 1),
-            Direction::WEST  => (self.x - 1, self.y),
-            Direction::EAST  => (self.x + 1, self.y),
-            _                => (self.x, self.y)
+            'N' => (self.x, self.y - 1),
+            'S' => (self.x, self.y + 1),
+            'W' => (self.x - 1, self.y),
+            'E' => (self.x + 1, self.y),
+            _   => (self.x, self.y)
         }
     }
 
-    fn proposed_direction(&mut self, c: &Vec<u8>) -> (isize, isize) {
-        while let Some(dir) = self.considered_directions.pop_front() {
+    fn proposed_direction(&mut self, c: &Vec<u8>, directions: &VecDeque<char>) -> char {
+        for dir in directions {
             let can_move = match dir {
-                Direction::NORTH => c[0] == 0 && c[1] == 0 && c[2] == 0,
-                Direction::SOUTH => c[5] == 0 && c[6] == 0 && c[7] == 0,
-                Direction::WEST  => c[1] == 0 && c[3] == 0 && c[5] == 0,
-                Direction::EAST  => c[2] == 0 && c[4] == 0 && c[7] == 0,
-                _ => false
+                'N' => c[0] == 0 && c[1] == 0 && c[2] == 0,
+                'S' => c[5] == 0 && c[6] == 0 && c[7] == 0,
+                'W' => c[1] == 0 && c[3] == 0 && c[5] == 0,
+                'E' => c[2] == 0 && c[4] == 0 && c[7] == 0,
+                _   => false
             };
 
-            let copy = dir.clone();
-            self.considered_directions.push_back(dir);
             if can_move {
-                println!("PROPOSE DADDY: {} {:?}", self.id, copy);
-                return self.to_coords(copy)
+                return *dir
             }
         }
 
-        (self.x, self.y)
-
+        'I'
     }
 }
-
-type Point = Elf;
 
 fn main() {
     println!("P1: {}", part1("input"));
     println!("P2: {}", part2("input"));
 }
 
-fn part1(file: &'static str) -> isize {
+
+fn part1(file: &'static str) -> usize {
     let mut elfs = parse(file);
+    let mut considered_directions = VecDeque::from(['N', 'S', 'W', 'E']);
+
     let empty = vec![0; TRANSFORMATIONS.len()];
+    let mut counter = 0;
 
     loop {
-        println!("\nLOOP CYCLE===========");
         let mut move_set: HashMap<(isize, isize), Vec<usize>> = HashMap::new();
         let mut combs: HashMap<usize, Vec<u8>> = HashMap::new();
 
@@ -111,40 +98,64 @@ fn part1(file: &'static str) -> isize {
 
         for elf in &mut elfs {
             let c = &combs[&elf.id];
+
             if c == &empty {
                 continue
             }
 
-            let (elf_dx, elf_dy) = elf.proposed_direction(c);
+            let direction = elf.proposed_direction(c, &considered_directions);
+            let (elf_dx, elf_dy) = elf.to_coords(&direction);
+
             move_set
                 .entry((elf_dx, elf_dy))
-                .and_modify(|mut value| value.push(elf.id))
+                .and_modify(|value| value.push(elf.id))
                 .or_insert(vec![elf.id]);
         }
+
+        considered_directions.rotate_left(1);
 
         move_set.retain(|_, v| v.len() < 2);
 
         for elf in &mut elfs {
-            println!("ELF STATE {:?}", elf);
             for ((x, y), elf_ids) in &move_set {
                 if elf.id == elf_ids[0] {
-                    println!("THIS ELF ACTUALLY MOVED {}", elf.id);
                     elf.x = *x;
                     elf.y = *y;
                 }
             }
         }
 
-        println!("{:?}", move_set);
+        counter += 1;
+
+        if counter == 10 {
+            break;
+        }
     }
-    //println!("{:?}", elfs);
-    0
+
+    empty_ground_tiles(&elfs)
+}
+
+fn empty_ground_tiles(elfs: &Vec<Elf>) -> usize {
+    let mut count = 0;
+    let min_x = elfs.iter().map(|e| e.x).min().unwrap();
+    let max_x = elfs.iter().map(|e| e.x).max().unwrap();
+    let min_y = elfs.iter().map(|e| e.y).min().unwrap();
+    let max_y = elfs.iter().map(|e| e.y).max().unwrap();
+
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            if elfs.iter().find(|e| e.x == x && e.y == y).is_none() {
+                count += 1
+            }
+        }
+    }
+    count
 }
 
 #[test]
 fn test_part1() {
-    assert_eq!(part1("test_input2"), 1);
-    //assert_eq!(part1("test_input"), 1);
+    assert_eq!(part1("test_input"), 110);
+    assert_eq!(part1("test_input2"), 25);
 }
 
 fn part2(file: &'static str) -> isize {
@@ -170,13 +181,7 @@ fn parse(file: &'static str) -> Vec<Elf> {
                 Elf {
                     id: elf_id,
                     x: x as isize,
-                    y: y as isize,
-                    considered_directions: VecDeque::from([
-                        Direction::NORTH,
-                        Direction::SOUTH,
-                        Direction::WEST,
-                        Direction::EAST
-                    ])
+                    y: y as isize
                 }
             );
             elf_id += 1;
