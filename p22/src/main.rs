9355db7c2139a67720af679489f892104e7b012c
grdw <gerard@grdw.nl>
2022-12-23 10:56:05 +0100

    Yeah whatever

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 struct Point {
     x: isize,
     y: isize,
+    face: usize,
     wall: bool
 }
 
@@ -16,11 +17,25 @@ fn main() {
 }
 
 fn part1(file: &'static str) -> isize {
+    traverse_path(file, walk)
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1("test_input"), 6032);
+}
+
+fn traverse_path(
+    file: &'static str,
+    walk: fn(map: &MonkeyMap, start: &mut Point, steps: u8, dir: char))
+    -> isize
+{
     let (map, instr) = parse(file);
     let mut dir = '>';
     let mut start = Point {
         x: map[0].x,
         y: map[0].y,
+        face: map[0].face,
         wall: false
     };
 
@@ -45,9 +60,61 @@ fn part1(file: &'static str) -> isize {
     };
 
     (1000 * (start.y + 1)) + (4 * (start.x + 1)) + facing_value
+
 }
 
 fn walk(map: &MonkeyMap, start: &mut Point, steps: u8, dir: char) {
+    let n_min_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).min().unwrap();
+    let n_max_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).max().unwrap();
+    let n_min_x = map.iter().filter(|p| p.y == start.y).map(|p| p.x).min().unwrap();
+    let n_max_x = map.iter().filter(|p| p.y == start.y).map(|p| p.x).max().unwrap();
+    let height = n_max_y;
+    let width = n_max_x;
+
+    for _ in 0..steps {
+        let (mut nx, mut ny): (isize, isize) = match dir {
+            'v' => (0, 1),
+            '>' => (1, 0),
+            '<' => (-1, 0),
+            '^' => (0, -1),
+            _ => panic!("BOOM!")
+        };
+
+        if start.x + nx >= 0 {
+            nx += start.x
+        } else if start.x + nx >= n_max_x {
+            nx += n_min_x
+        } else if start.x + nx <= n_min_x {
+            nx += n_max_x
+        }
+
+        if start.y + ny >= 0 {
+            ny += start.y
+        } else if start.y + ny >= n_max_y {
+            ny += n_min_y
+        } else if start.y + ny <= n_min_y {
+            ny += n_max_y
+        }
+
+        nx %= width;
+        ny %= height;
+
+        let is_wall = map
+            .iter()
+            .find(|p| p.x == nx && p.y == ny && p.wall)
+            .is_some();
+
+        if is_wall {
+            break;
+        }
+
+        start.x = nx;
+        start.y = ny;
+
+    }
+}
+
+fn walk_cube(map: &MonkeyMap, start: &mut Point, steps: u8, dir: char) {
     let n_min_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).min().unwrap();
     let n_max_y = map.iter().filter(|p| p.x == start.x).map(|p| p.y).max().unwrap();
     let n_min_x = map.iter().filter(|p| p.y == start.y).map(|p| p.x).min().unwrap();
@@ -95,18 +162,13 @@ fn walk(map: &MonkeyMap, start: &mut Point, steps: u8, dir: char) {
     }
 }
 
-#[test]
-fn test_part1() {
-    assert_eq!(part1("test_input"), 6032);
-}
-
-fn part2(file: &'static str) -> usize {
-    0
+fn part2(file: &'static str) -> isize {
+    traverse_path(file, walk_cube)
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2("test_input"), 1);
+    assert_eq!(part2("test_input"), 5031);
 }
 
 fn parse(file: &'static str) -> (MonkeyMap, Instructions) {
@@ -114,14 +176,26 @@ fn parse(file: &'static str) -> (MonkeyMap, Instructions) {
 
     let mut dir_map = vec![];
     let mut parsed_instructions = vec![];
-    let (map, mut instructions) = text.split_once("\n\n").unwrap();
-    for (y, line) in map.split("\n").enumerate() {
+    let (map, instructions) = text.split_once("\n\n").unwrap();
+
+    let lines: Vec<&str> = map.split("\n").collect();
+    let cube_edge_size = lines[0].trim().len();
+    let mut c = 0;
+    //println!("{}", cube_edge_size);
+    for (y, line) in lines.iter().enumerate() {
         for (x, p) in line.chars().enumerate() {
             if p == ' ' { continue }
 
             dir_map.push(
-                Point { y: y as isize, x: x as isize, wall: p == '#' }
+                Point {
+                    y: y as isize,
+                    x: x as isize,
+                    face: c / cube_edge_size.pow(2),
+                    wall: p == '#'
+                }
             );
+
+            c += 1;
         }
     }
 
