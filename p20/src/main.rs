55f9e92fd289cb808ac971c8ee3dbe6c2a900e6b
grdw <gerard@grdw.nl>
2022-12-23 13:02:25 +0100

    Setup for d20 p1

--- p20/src/main.rs
+++ p20/src/main.rs
@@ -6,18 +6,28 @@ fn main() {
 }
 
 fn part1(file: &'static str) -> usize {
+    let nums = parse_file(file);
     0
 }
 
+#[test]
 fn test_part1() {
-    assert_eq!(part1("test_input"), 1);
+    assert_eq!(part1("test_input"), 3);
 }
 
 fn part2(file: &'static str) -> usize {
     0
 }
 
+#[test]
 fn test_part2() {
     assert_eq!(part2("test_input"), 1);
 }
 
+fn parse_file(file: &'static str) -> Vec<i32> {
+    let contents = fs::read_to_string(file).unwrap();
+    contents
+        .split_terminator("\n")
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect()
+}
