90251a7e6492258a0671523645c340ff720b33a5
grdw <gerard@grdw.nl>
2022-12-26 20:30:04 +0100

    Drop the flow rate from dijkstra

--- p16/src/main.rs
+++ p16/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs;
 use std::collections::{HashMap, BinaryHeap};
-use permutator::{Permutation};
+use std::process::Command;
 use std::cmp::Ordering;
 use regex::Regex;
 
 #[derive(Debug, Clone)]
-struct Edge(usize, usize);
+struct Edge(usize);
 
-#[derive(Debug, Eq, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq)]
 struct Valve {
     id: usize,
     name: String,
@@ -17,7 +17,6 @@ struct Valve {
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 struct State {
     time: usize,
-    flow_rate: usize,
     position: usize,
 }
 
@@ -51,16 +50,15 @@ fn dijkstra(edges: &Edges, start: usize, goal: usize) -> Option<State> {
     let mut heap = BinaryHeap::new();
 
     dist[start] = 0;
-    heap.push(State { time: 0, flow_rate: 0, position: start });
+    heap.push(State { time: 0, position: start });
 
-    while let Some(State { time, flow_rate, position }) = heap.pop() {
-        if position == goal { return Some( State { time, flow_rate, position }); }
+    while let Some(State { time, position }) = heap.pop() {
+        if position == goal { return Some( State { time, position }); }
         if time > dist[position] { continue; }
 
         for edge in &edges[position] {
             let next = State {
                 time: time + 1,
-                flow_rate: edge.1 * (30 - time - 1),
                 position: edge.0
             };
 
@@ -77,49 +75,43 @@ fn dijkstra(edges: &Edges, start: usize, goal: usize) -> Option<State> {
 
 fn part1(file: &'static str) -> usize {
     let (mut valves, edges) = parse(file);
+    let mut cached_distances = HashMap::new();
     let mut max_flow = 0;
-    let mut map = HashMap::new();
+    let start_id = valves.iter().find(|v| v.name == "AA".to_string()).unwrap().id;
 
     valves.retain(|v| v.flow_rate > 0 || v.name == "AA".to_string());
 
-    for valve in &valves {
-        for other_valve in &valves {
-            if valve.id == other_valve.id { continue }
-
-            let key = format!("{}-{}", valve.id, other_valve.id);
-            let dijk = dijkstra(&edges, valve.id, other_valve.id).unwrap();
-            map.insert(key, dijk.time + 1);
+    for v in &valves {
+        for ov in &valves {
+            let key = format!("{}-{}", v.id, ov.id);
+            cached_distances.insert(key, dijkstra(&edges, v.id, ov.id).unwrap());
         }
     }
 
-    valves.permutation().for_each(|mut perm| {
-        let mut current = 0;
-        let mut total_flow_rate = 0;
-        let mut minutes = 30;
+    valves.retain(|v| v.flow_rate > 0);
 
-        while let Some(valve) = perm.pop() {
-            if current == valve.id { continue }
+    println!("{:?}", cached_distances);
+    //let mut current = start_id;
+    //let mut total_flow_rate = 0;
+    //let mut minutes = 30;
 
-            let key = format!("{}-{}", current, valve.id);
-            let travel_time = map[&key];
-            if minutes < travel_time {
-                break;
-            }
-            minutes -= travel_time;
-            total_flow_rate += valve.flow_rate * minutes;
-            current = valve.id;
-        }
+    //while let Some(valve) = perm.pop() {
+    //    if current == valve.id { continue }
 
-        if total_flow_rate > max_flow {
-            println!("{:?}", total_flow_rate);
-            max_flow = total_flow_rate
-        }
-    });
+    //    let key = format!("{}-{}", current, valve.id);
+    //    let travel_time = cached_distances[&key];
+    //    if minutes < travel_time {
+    //        break;
+    //    }
+    //    minutes -= travel_time;
+    //    total_flow_rate += valve.flow_rate * minutes;
+    //    current = valve.id;
+    //}
 
     max_flow
 }
 
-fn debug(valves: &Valves) {
+fn debug(valves: Vec<&Valve>) {
     println!("{:?}", valves.iter().map(|n| &n.name).collect::<Vec<&String>>());
     println!("");
 }
@@ -170,7 +162,7 @@ fn parse(file: &'static str) -> (Valves, Edges) {
     for (id, kids) in map {
         for kid in kids {
             let v = valves.iter().find(|v| v.name == kid).unwrap();
-            edges[id].push(Edge(v.id, v.flow_rate));
+            edges[id].push(Edge(v.id));
         }
     }
 
